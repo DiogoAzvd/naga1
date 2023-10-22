78cc015095558a4f16d3434d89b872dab43f56c3
Diogo <diogoazvd77@gmail.com>
2023-10-22 15:44:10 -0400

    deleted:    app/favicon.ico 	deleted:    app/globals.css 	deleted:    app/layout.js 	deleted:    app/page.js 	deleted:    app/page.module.css 	new file:   files/css/index.css 	new file:   files/images/Hangul_Memorizer.png 	new file:   files/images/Moonlight - Copia.png 	new file:   files/images/Moonlight.png 	new file:   files/images/Moonlight1.png 	new file:   files/images/discord_logo.png 	new file:   files/images/lupa.png 	new file:   files/images/moon_icon.png 	new file:   pages/_app.js 	new file:   pages/index.jsx 	deleted:    public/next.svg 	deleted:    public/vercel.svg

---
+++ pages/index.jsx
@@ -0,0 +1,122 @@
+export default function Main() {
+    return (
+        <>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Patrick Hand"></link>
+            <Introduction />
+            <Project />
+        </>        
+    );
+}
+
+
+function Introduction() {
+    let turn_on_off = 1;
+
+    function copy_to_clipboard() {
+        if (turn_on_off == 1) {
+            turn_on_off = 0;
+            let text = "dummy_naga#0";
+            navigator.clipboard.writeText(text);
+
+            const clipboard_box = document.querySelector(".clipboard");
+            clipboard_box.classList.add("show_hide");
+
+            setTimeout(() => {
+                clipboard_box.classList.remove("show_hide");
+                
+                setTimeout(() => {
+                    turn_on_off = 1;
+                }, 1200)
+            }, 2000)
+        }
+    }
+
+    return (
+        <div id="introduction">
+            <div id="intro_block">
+                <div id="title_block">
+                    <div id="title">Welcome!</div>
+                </div>
+
+                <div id="text_block">
+                    <div id="text">You can call me Naga. I will be posting my projects here. Mainly working with React, JS, HTML, CSS and Next.js.</div>
+                </div>
+
+                <div id="contact_block">
+                    <div id="discord_img" onClick={copy_to_clipboard}></div>
+                    <div id="discord" onClick={copy_to_clipboard}>dummy_naga#0</div>
+                    <div className="clipboard">Copied to clipboard!</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+function Project() {
+    return (
+        <div id="project">
+            <Moon_project class_name = "hangul_memorizer" title = "Hangul Memorizer" text = "Hangul is the name of the korean alphabet! The purpose of the site is to help memorize it while playing a minigame, where it is displayed a korean letter and you need to select its romanized version."/>
+
+            <div id="project_block">
+                <div id="project_title">Project List</div>
+                <div id="project_tip">Click on an item to display image and description.</div>
+                <div id="project_list">
+                    <Moon_list text = "Hangul Memorizer" project_class = ".hangul_memorizer"/>
+                    <div>Placeholder</div>
+                    <div>Placeholder</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+function Moon_list(data) {
+    function hide_project_list() {
+        const project_list = document.getElementById("project_block");
+        project_list.style.display = "none";
+
+        const project = document.querySelector(data["project_class"]);
+        project.style.display = "flex";
+    }
+
+    return (
+        <div id="moon_list">
+            <div id="moon_icon"></div>
+            <div id="moon_text" onClick={hide_project_list}>{data["text"]}</div>
+        </div>
+    );
+}
+
+
+function Moon_project(data) {
+    function hide() {
+        const element = document.querySelector("." + data["class_name"]);
+        element.style.display = "none";
+
+        const project_list = document.getElementById("project_block");
+        project_list.style.display = "flex";
+    }
+
+    return (
+        <div className={`${data["class_name"]} moon_project`}>
+            <div className="moon_block_l">
+                <div className="moon_project_title">
+                    {data["title"]}
+                    <div className="close_moon_project" onClick={hide}>X</div>
+                </div>
+
+                <div className="moon_project_img_block">
+                    <div className="moon_project_img"></div>
+                </div>                 
+            </div>
+
+            <div className="moon_block_r">
+                <div className="moon_project_text">
+                    {data["text"]}
+                </div>
+            </div>
+        </div>
+    );
+}
